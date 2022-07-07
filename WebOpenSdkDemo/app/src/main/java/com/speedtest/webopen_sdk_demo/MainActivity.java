295eb7dd833c9fa085fd72355abcb6e1d95cd543
pql <pengqinglongbest@163.com>
2022-07-07 18:27:27 +0800

    网页打开gradle方式依赖

--- WebOpenSdkDemo/app/src/main/java/com/speedtest/webopen_sdk_demo/MainActivity.java
+++ WebOpenSdkDemo/app/src/main/java/com/speedtest/webopen_sdk_demo/MainActivity.java
@@ -2,13 +2,26 @@ package com.speedtest.webopen_sdk_demo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnWebOpen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnWebOpen = findViewById(R.id.btn_webopen);
+
+        btnWebOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, WebOpenActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ WebOpenSdkDemo/app/src/main/java/com/speedtest/webopen_sdk_demo/SDKConfig.java
@@ -0,0 +1,7 @@
+package com.speedtest.webopen_sdk_demo;
+
+public class SDKConfig {
+
+  public static String APP_ID = "configure your appID here";
+  public static String APP_KEY = "configure your appKEY here";
+}
