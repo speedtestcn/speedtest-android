37714712ae0c90e9d9d72c4eeb936152f9601231
pql <pengqinglongbest@163.com>
2022-07-07 16:38:32 +0800

    初始化优化

--- SpeedTestSDKDemo/app/src/main/java/com/speedtest/speedtest_sdk/MyApplication.java
+++ SpeedTestSDKDemo/app/src/main/java/com/speedtest/speedtest_sdk/MyApplication.java
@@ -11,7 +11,7 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-//        SpeedtestInterface.init(this, SDKConfig.APP_ID, SDKConfig.APP_KEY);
+        SpeedtestInterface.init(this, SDKConfig.APP_ID, SDKConfig.APP_KEY);
     }
 
     @Override
