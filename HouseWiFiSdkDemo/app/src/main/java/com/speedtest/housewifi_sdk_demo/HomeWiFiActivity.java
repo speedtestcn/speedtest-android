a43ff69944394af0703d436f3af9756619671717
pql <pengqinglongbest@163.com>
2022-07-11 16:43:48 +0800

    全屋WiFi sdk demo

---
+++ HouseWiFiSdkDemo/app/src/main/java/com/speedtest/housewifi_sdk_demo/HomeWiFiActivity.java
@@ -0,0 +1,14 @@
+package com.speedtest.housewifi_sdk_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeWiFiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_wi_fi);
+    }
+}
