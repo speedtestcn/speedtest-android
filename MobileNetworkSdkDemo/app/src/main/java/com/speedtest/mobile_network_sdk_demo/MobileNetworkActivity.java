751831609181379b4c381f8917ab44260f439b37
pql <pengqinglongbest@163.com>
2022-07-11 15:20:25 +0800

    移动网络检测sdk demo

---
+++ MobileNetworkSdkDemo/app/src/main/java/com/speedtest/mobile_network_sdk_demo/MobileNetworkActivity.java
@@ -0,0 +1,14 @@
+package com.speedtest.mobile_network_sdk_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MobileNetworkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mobile_network);
+    }
+}
