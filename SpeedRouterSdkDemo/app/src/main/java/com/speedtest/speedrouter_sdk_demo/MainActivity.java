54db557e580c68774dc9896310f3a744f7d66977
pql <pengqinglongbest@163.com>
2023-02-23 19:08:44 +0800

    三段测速demo

---
+++ SpeedRouterSdkDemo/app/src/main/java/com/speedtest/speedrouter_sdk_demo/MainActivity.java
@@ -0,0 +1,26 @@
+package com.speedtest.speedrouter_sdk_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SpeedRouterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
